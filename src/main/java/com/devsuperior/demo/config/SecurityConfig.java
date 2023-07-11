59a0ef4d065ff26aa34a7a50dcf15c7b5fe16aa4
DennerOl <dener155@hotmail.com>
2023-07-10 21:44:02 -0300

    BCrypt-PasswordEncoder

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,11 +3,22 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+	
+	
+/* gera um codigo hash ou seja criptografa a senha do
+ * usuario que estava direto no  banco de dados 
+ */
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -18,5 +29,7 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	
+	
 }
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$VnEwLdnNGXBrkD9mxmiA6uj9oitVx6/e/Ip9I2Ik0ebOz65EoE6EW');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$VnEwLdnNGXBrkD9mxmiA6uj9oitVx6/e/Ip9I2Ik0ebOz65EoE6EW');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
