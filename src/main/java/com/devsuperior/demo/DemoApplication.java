e49ae455e3f20bf6549e3b2873aebfe1bd18f89e
DennerOl <dener155@hotmail.com>
2023-07-12 22:17:49 -0300

    Requisição de login

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,24 +1,24 @@
 package com.devsuperior.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner{
+public class DemoApplication {
 
+/*	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+*/	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
+	/*
 // chamo o metodo de configuração para fazer o teste
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Encoder = "+ passwordEncoder.encode("123456"));
 	}
-
+*/
 }
